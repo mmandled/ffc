863b475c7b6900714f752a8afe2e3334cc25e2c4
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-05 13:47:08 +0800

    Push new activity but not yet finish

---
+++ JavaScript FFC/chatbot.js
@@ -0,0 +1,22 @@
+console.log("Hi there!");
+
+const botName = "teacherBot";
+
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+const subject = "JavaScript";
+const topic = "strings";
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
+
+const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
+
+console.log(subject.length);
+
+console.log(`Here is an example of using the length property on the word ${topic}.`);
+console.log(topic.length);
+
+console.log(`Here is an example of accessing the  first letter in the word ${subject}.`);
