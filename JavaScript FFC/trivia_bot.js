b9cf0f91c4fdf858e6019c3705262b9816a68190
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-05 12:04:30 +0800

    Added new lab activity

---
+++ JavaScript FFC/trivia_bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "botbot";
+let botLocation = "from universe";
+let favoriteLanguage = 'C';
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Facts about my " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "My language is cute " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "This should be fun " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
