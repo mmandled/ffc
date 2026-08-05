ec3775cc4710ffaec4ae1f40902b20556b6fee5a
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-05 12:21:52 +0800

    Added sentence maker activity

---
+++ JavaScript FFC/sentence_make.js
@@ -0,0 +1,19 @@
+    let adjective = "wow";
+    let noun = "wew";
+    let verb = "dance";
+    let place = "Japan";
+    let adjective2 = "nice";
+    let noun2 = "okay";
+
+    let firstStory = "Once upon a time, there was a(n) " + adjective + " " +  noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+    console.log("First story: " + firstStory);
+
+    adjective = "this";
+    noun = "cute";
+    verb = "go";
+    place = "Cebu";
+    adjective2 = "there";
+    noun2 = "apple";
+
+    let secondStory = "Once upon a time, there was a(n) " + adjective + " " +  noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+    console.log("Second story: " + secondStory);
