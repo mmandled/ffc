6f5770f667145fffd30a1d859d661a8524a65526
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-06 11:32:24 +0800

    fix test in ffc lab

---
+++ JavaScript FFC/string_inspector.js
@@ -0,0 +1,5 @@
+const fccSentence = "freeCodeCamp is a great place to learn web development.";
+console.log("Here are some examples of the includes() method:");
+
+const hasFreeCodeCamp = fccSentence.includes("freeCodeCamp");
+console.log(`ffcSentence.includes("freeCodeCamp") returns ${hasFreeCodeCamp} because the word "freeCodeCamp" is in the sentence.`);
