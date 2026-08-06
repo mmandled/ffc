898821d96136fc8503131a40a38547f00628d6aa
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-06 13:41:01 +0800

    string format activity complete

--- JavaScript FFC/string_format.js
+++ JavaScript FFC/string_format.js
@@ -13,3 +13,18 @@ console.log(trimmedStart);
 const trimmedEnd = userInput.trimEnd();
 console.log("After using the trimEnd() method, trailing spaces removed:");
 console.log(trimmedEnd);
+
+const upperCaseInput = cleanedInput.toUpperCase();
+console.log("Result of using the toUpperCase() method:");
+console.log(upperCaseInput);
+
+const lowerCaseInput = cleanedInput.toLowerCase();
+console.log("Result of using the toLowerCase() method:");
+console.log(lowerCaseInput);
+
+const lowercaseWord = "camelcase";
+const camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase() + lowercaseWord.slice(-3);
+console.log("Camel cased version:");
+console.log(camelCasedVersion);
+
+
