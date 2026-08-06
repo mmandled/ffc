2ffc00073b0fe4aae899072089861eb699ec997f
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-06 11:00:34 +0800

    Added chatbot test activity

--- JavaScript FFC/chatbot.js
+++ JavaScript FFC/chatbot.js
@@ -20,3 +20,26 @@ console.log(`Here is an example of using the length property on the word ${topic
 console.log(topic.length);
 
 console.log(`Here is an example of accessing the  first letter in the word ${subject}.`);
+
+console.log(subject[0]);
+
+console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
+
+console.log(subject[1]);
+
+console.log(`Here is an example of accessing the last letter in the word ${subject}.`);
+
+console.log(subject[9]);
+
+const lastCharacter = subject[subject.length - 1];
+console.log(lastCharacter);
+
+const learningIsFunSentence = "Learning is fun.";
+console.log("Here are examples of finding the positions of substrings in the sentence.");
+
+console.log(learningIsFunSentence.indexOf("Learning"));
+
+console.log(learningIsFunSentence.indexOf("fun"));
+console.log(learningIsFunSentence.indexOf("learning"));
+
+console.log("I hope you enjoyed learning today.");
