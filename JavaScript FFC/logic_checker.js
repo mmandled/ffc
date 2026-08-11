3169484f233c5452f9bebf304a94fc45652bbbe9
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-12 00:04:02 +0800

    Done with the logic checker activity

--- JavaScript FFC/logic_checker.js
+++ JavaScript FFC/logic_checker.js
@@ -1,1 +1,17 @@
-const truthyOrFalsy = true;
+const hasDeveloperJob = true;
+const isTimmyAGamer = false;
+const timmyAge = 15;
+
+if(hasDeveloperJob){
+    console.log("Timmy is employed as a developer.");
+}
+
+if(isTimmyAGamer){
+    console.log("Timmy loves to play World of Warcraft.");
+}
+
+if(timmyAge >= 16){
+    console.log("Timmy is old enough to drive.");
+} else {
+    console.log("Timmy is not old enough to drive.");
+}
