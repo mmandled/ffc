320b380b56fb6c646e1b93fad471e7348db824f3
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-13 20:25:29 +0800

    Done with fortune teller lab activity

--- JavaScript FFC/fortune_teller.js
+++ JavaScript FFC/fortune_teller.js
@@ -4,3 +4,28 @@ const fortune3 = "Be cautious of your new neighbors.";
 const fortune4 = "You will find a new hobby soon.";
 const fortune5 = "It would be wise to avoid the color red today.";
 
+
+const randomNumber = Math.floor(Math.random() * 5) + 1;
+
+console.log(randomNumber);
+
+let selectedFortune;
+
+if(randomNumber == 1){
+    selectedFortune = fortune1;
+    console.log(selectedFortune);
+} else if(randomNumber == 2){
+    selectedFortune = fortune2;
+    console.log(selectedFortune);
+} else if(randomNumber == 3){
+    selectedFortune = fortune3;
+    console.log(selectedFortune);
+} else if(randomNumber == 4){
+    selectedFortune = fortune4;
+    console.log(selectedFortune);
+} else if(randomNumber == 5){
+    selectedFortune = fortune5;
+    console.log(selectedFortune);
+} else{
+    console.log("Empty");
+}
