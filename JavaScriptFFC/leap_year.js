be379448ce1b140dc62fe8169d23316b40b80b6a
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-19 18:50:38 +0800

    Finished with leap year

--- JavaScriptFFC/leap_year.js
+++ JavaScriptFFC/leap_year.js
@@ -1,14 +1,14 @@
 let year = 0;
 
-function isLeapYear() {
-  if (x / 4) {
-    return `[${year}] is a leap year`;
-  } else if (x / 100) {
-    return `[${year}] is not a leap year`;
-  } else if (x / 400) {
-    return `[${year}] is a leap year`;
+function isLeapYear(x) {
+  if (x % 400 == 0) {
+    return `${x} is a leap year.`;
+  } else if (x % 100 == 0) {
+    return `${x} is not a leap year.`;
+  } else if (x % 4 == 0) {
+    return `${x} is a leap year.`;
   }
 }
 
 let result = isLeapYear(year);
-console.log(isLeapYear(result));
+console.log(result);
