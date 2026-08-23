16ad4f526d2adf56fda05f4138ce15ece5115640
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-23 22:55:16 +0800

    Finished golf score lab test

--- JavaScriptFFC/golf_score.js
+++ JavaScriptFFC/golf_score.js
@@ -1,1 +1,20 @@
 const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, stroke){
+    if(stroke == 1){
+        return "Hole-in-one!";
+    }else if(stroke <= par - 2){
+        return "Eagle";
+    }else if(stroke == par - 1){
+        return "Birdie";
+    }else if(stroke == par){
+        return "Par";
+    }else if(stroke == par + 1){
+        return "Bogey";
+    }else if(stroke == par + 2){
+        return "Double Bogey";
+    }else if(stroke >= par + 3){
+        return "Go Home!";
+    }else return "Hays";
+}
+
