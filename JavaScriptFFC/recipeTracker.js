710cd58958c44a8af5fee478a2fd70bc2dbe523c
Emmanuel <emmanuelcabizares@gmail.com>
2026-09-01 16:28:44 +0800

    Added Recipe Tracker

---
+++ JavaScriptFFC/recipeTracker.js
@@ -0,0 +1,34 @@
+const recipes = [];
+
+const recipe1 = {
+    name: "Spaghetti Carbonara",
+    ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+    cookingTime: 22,
+    totalIngredients: null,
+    difficultyLevel: "",
+};
+
+const recipe2 = {
+    name: "Chicken Curry",
+    ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+    cookingTime: 42,
+    totalIngredients: null,
+    difficultyLevel: "",
+};
+
+const recipe3 = {
+    name: "Vegetable Stir Fry",
+    ingredients: ["broccoli", "carrot", "bell pepper"],
+    cookingTime: 15,
+    totalIngredients: null,
+    difficultyLevel: "",
+};
+
+const recipe1Name = recipe1.name;
+console.log(recipe1Name);
+
+const recipe2CookingTime = recipe2.cookingTime;
+console.log(recipe2CookingTime);
+
+const recipe3Ingredients = recipe3.ingredients;
+console.log(recipe3Ingredients);
